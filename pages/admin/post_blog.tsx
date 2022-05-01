babfe0ae7ccedc78d861d1b6786f0ef022d46c8e
Beedhan <45665443+Beedhan@users.noreply.github.com>
2022-05-01 23:32:46 +0545

    added blog and user

--- pages/admin/post_blog.tsx
+++ pages/admin/post_blog.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 import dynamic from "next/dynamic";
@@ -7,15 +7,21 @@ import style from "../../components/admin/EditorPage.module.css";
 import supabase from "../../utils/SupabaseClient";
 import toast, { Toaster } from "react-hot-toast";
 import { useRouter } from "next/router";
-const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
+import "react-markdown-editor-lite/lib/index.css";
+import MarkdownIt from "markdown-it";
+
+const MDEditor = dynamic(() => import("react-markdown-editor-lite"), {
+  ssr: false,
+});
 
 const PostBlog: NextPage = () => {
-  const [value, setValue] = useState("**Hello world!!!**");
+  const [value, setValue] = useState<string>("**Hello world!!!**");
   const [title, setTitle] = useState("Title");
   const [tag, setTag] = useState("Tag");
   const [description, setDescription] = useState("Description");
 
   const router = useRouter();
+  const mdParser = new MarkdownIt(/* Markdown-it options */);
 
   useEffect(() => {
     const user = supabase.auth.user();
@@ -44,6 +50,10 @@ const PostBlog: NextPage = () => {
         });
     console.log(data, error);
   };
+
+  const handleMd = ({ html, text }: { html: string; text: string }) => {
+    setValue(text);
+  };
   return (
     <div className={style.container}>
       <form className={style.inputContainer}>
@@ -68,7 +78,12 @@ const PostBlog: NextPage = () => {
           onChange={(e) => setTag(e.target.value)}
           required
         />
-        <MDEditor value={value} onChange={setValue} />
+        <MDEditor
+          onChange={handleMd}
+          style={{ height: "300px" }}
+          value={value}
+          renderHTML={(text: string) => mdParser.render(text)}
+        />
         <button
           type="submit"
           className={style.postButton}
