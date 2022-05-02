11de2803b4ab537ae72381ef57b90b2380784ebb
Beedhan <45665443+Beedhan@users.noreply.github.com>
2022-05-02 21:11:23 +0545

    added more styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,8 +29,8 @@ function MyApp({ Component, pageProps, router }: AppProps) {
             },
             pageExit: {
               opacity: 0,
-              x: 100,
-              y: 0,
+              y: 100,
+              x: 0,
             },
           }}
         >
