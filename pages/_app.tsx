6b86fde827690a8fb405a8a8cb3eb796adadafa1
Beedhan <45665443+Beedhan@users.noreply.github.com>
2022-05-02 16:43:48 +0545

    added page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,31 @@ import type { AppProps } from "next/app";
 import Navbar from "../components/home/nav/navbar";
 import Footer from "../components/footer/Footer";
 import { Toaster } from "react-hot-toast";
-
-function MyApp({ Component, pageProps }: AppProps) {
+import { AnimatePresence, motion } from "framer-motion";
+function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <>
+    <AnimatePresence>
       <Toaster />
       <Navbar />
-      <Component {...pageProps} />
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+            y: "100px",
+          },
+          pageAnimate: {
+            opacity: 1,
+            y: "0px",
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
       <Footer />
-    </>
+    </AnimatePresence>
   );
 }
 
